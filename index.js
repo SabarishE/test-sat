14741c46ddee5708191067fa20f8efdd73e99b40
SabarishE <sabarishtrblzr@gmail.com>
2021-07-24 11:06:18 +0530

    newcommit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import express, { response } from "express";
 const app=express();
 
       
-app.listen(3000,console.log("server started"));
+app.listen(3000,console.log("server started now"));
 
 app.get("/",(req,res)=>{
     res.send("test !!!!");
